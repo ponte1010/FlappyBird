5be5ad5475fe8a0474b3e8d9f902f9eaace53180
ponte1010 <t.miwa.528@nitech.jp>
2020-11-27 17:22:43 +0900

    [add]リスタート処理

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -258,11 +258,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // 画面をタップした時に呼ばれる
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // 鳥の速度をゼロにする
-        bird.physicsBody?.velocity = CGVector.zero
-        
-        // 鳥に縦方向の力を与える
-        bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 15))
+        if scrollNode.speed > 0 {
+            // 鳥の速度をゼロにする
+            bird.physicsBody?.velocity = CGVector.zero
+            
+            // 鳥に縦方向の力を与える
+            bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 15))
+            
+        } else if bird.speed == 0 {
+            restart()
+        }
     }
     
     // SKPhysicsContactDelegateのメソッド。衝突したときに呼ばれる
@@ -290,5 +295,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 self.bird.speed = 0
             })
         }
+    }
+    
+    func restart() {
+        score = 0
+        
+        bird.position = CGPoint(x: self.frame.size.width * 0.2, y:self.frame.size.height * 0.7)
+        bird.physicsBody?.velocity = CGVector.zero
+        bird.physicsBody?.collisionBitMask = groundCategory | wallCategory
+        bird.zRotation = 0
+        
+        wallNode.removeAllChildren()
+        
+        bird.speed = 1
+        scrollNode.speed = 1
     }
 }
